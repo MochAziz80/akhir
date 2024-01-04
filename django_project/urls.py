27e5dd5d0e4171e64c839e059d3958d23c33a003
MochAziz80 <114449451+MochAziz80@users.noreply.github.com>
2024-01-04 16:07:32 +0000

    Testing 1

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ashoes import views as ashoes_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ashoes_views.home)
 ]
